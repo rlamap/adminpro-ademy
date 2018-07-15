1c9546daddc4dada5822fb470f295f0fa690e78a
rlamap <rlamap@hotmail.com>
2018-07-15 13:56:03 -0500

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.modules.ts
@@ -0,0 +1,28 @@
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.modules';
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule { }
