2eabe716b5226ad0ddaf03d773c0cb866e18fe41
rlamap <rlamap@hotmail.com>
2018-07-15 13:52:33 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ROUTES
+import { APP_ROUTES } from './app.routes';
+
+// MODULES
+import { PagesModule } from './pages/pages.modules';
+import { SharedModule } from './shared/shared.modules';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule,
+    SharedModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
